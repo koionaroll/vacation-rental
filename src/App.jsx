baa3a7440a0ca5d9fb3810168c979823be73dd5b
koionaroll <tranvankhoi2002@gmail.com>
2023-12-01 14:40:36 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Title = styled.div`
+  color:#BF4F74;
+`
+
+
+function App() {
+
+  return (
+    <>
+      <Title>hello world</Title>
+    </>
+  )
+}
+
+export default App
